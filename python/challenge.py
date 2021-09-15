47c3c77adbf1d14d5aa5084ff46959656eca1767
Carlos Herrera <cahe1540@gmail.com>
2021-09-15 08:54:55 -0700

    python challenge

--- python/challenge.py
+++ python/challenge.py
@@ -1,2 +1,20 @@
+import math
+
 #python question
-print("hello world!")
+def rollThicknessCM(foilLength: float) -> float:
+    tubeDiameter: float = 4.0
+    foilThickness: float = 0.0025
+    circumference = math.pi * tubeDiameter
+    while(foilLength > 0):
+        tubeDiameter += foilThickness * 2 
+        foilLength -= circumference
+        circumference = math.pi * tubeDiameter
+    
+    if((foilLength + circumference)/circumference < 0.5):
+        tubeDiameter -= foilThickness
+    return round(tubeDiameter,4)
+
+arr = [0, 50, 4321, 10000]
+
+arr = [rollThicknessCM(num) for num in arr]
+print(arr)
